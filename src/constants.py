a64152e59037144041da0bf3bd31c87a21461f70
Harinder Singh Sudwal <sudwalh@gmail.com>
2024-12-14 17:52:02 -0800

    constants file

---
+++ src/constants.py
@@ -0,0 +1,33 @@
+# BASE:
+PROJECT_NAME = "Loan Approval"
+AUTHOR = "Harinder Singh Sudwal"
+
+OUTPUT='output'
+
+# Data Ingestion
+TEST_SIZE = 0.2
+RAW_FOLDER='raw'
+TRAIN_DATA_FILE='train_data.csv'
+TEST_DATA_FILE='test_data.csv'
+
+# Data Validation
+THRESHOLD=0.05
+VALIDATE_FOLDER='validate'
+REPORT_VALIDATE_JSON='report.json'
+
+# Data Transformer
+TRANSFORMATION_FOLDER='transformation'
+X_TRAIN_PROCESS_DATA='x_train_process.csv'
+Y_TRAIN_PROCESS_DATA='y_train_process.csv'
+X_TEST_PROCESS_DATA='x_test_process.csv'
+Y_TEST_PROCESS_DATA='y_test_process.csv'
+
+# Model Trainer
+MODEL_LIST='model_list.csv'
+MODEL_FOLDER='model'
+BEST_MODEL_NAME='best_model.joblib'
+MODELS_FILE='model.csv'
+
+# Model Evaluation
+EVALUATION_FOLDER='evaluate'
+EVAL_JSON_NAME='model_eval_metrics.json'
